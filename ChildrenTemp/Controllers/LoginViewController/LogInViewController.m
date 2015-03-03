3f1b8f9cb0c7a814182b85c886a3678f98af07fd
JoyIos <646896350@qq.com>
2015-03-03 15:36:16 +0800

    register

--- ChildrenTemp/Controllers/LoginViewController/LogInViewController.m
+++ ChildrenTemp/Controllers/LoginViewController/LogInViewController.m
@@ -8,6 +8,7 @@
 
 #import "LogInViewController.h"
 #import "RegisterViewController.h"
+
 @interface LogInViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *loginButton;
 @property (weak, nonatomic) IBOutlet UITextField *userNameTextfield;
@@ -17,6 +18,7 @@
 
 @implementation LogInViewController
 - (IBAction)exitedFeild:(UITextField *)sender {
+   
 }
 - (IBAction)tapBgView:(UITapGestureRecognizer *)sender {
     [_passWordTextfield  resignFirstResponder];
@@ -37,8 +39,55 @@
     _loginButton.layer.masksToBounds = YES;
     [_loginButton setTitle:@"登 录" forState:UIControlStateNormal];
     [_loginButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [_loginButton addTarget:self action:@selector(tapLogInButton:) forControlEvents:UIControlEventTouchUpInside];
+}
+#pragma mark TextFiledDelegate
+- (BOOL)textFieldShouldClear:(UITextField *)textField{
+    return YES;
 }
 
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    if ([_userNameTextfield isFirstResponder]) {
+        [_userNameTextfield resignFirstResponder];
+        [_passWordTextfield becomeFirstResponder];
+    }else if ([_passWordTextfield isFirstResponder]){
+        [_passWordTextfield resignFirstResponder];
+    }
+    return YES;
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+    NSString *newString = [textField.text stringByReplacingCharactersInRange:range withString:string];
+    if (textField == _passWordTextfield){
+        if ([newString length] > 16) {
+            return NO;
+        }
+    }
+    return YES;
+}
+#pragma mark -登录
+-(void)tapLogInButton:(UIButton*)button{
+    
+    
+    NSLog(@"%@--------------%@",_userNameTextfield.text,_passWordTextfield.text);
+    if (!_userNameTextfield || ![_userNameTextfield.text length]) {
+        [HDM popHlintMsg:@"请输入手机号或邮箱!"];
+    }
+    if (![HDM isEmail:_userNameTextfield.text] && ![HDM isPhoneNum:_userNameTextfield.text]) {
+        [HDM popHlintMsg:@"请输入手机号或邮箱!"];
+        return;
+    }
+    if (!_userNameTextfield || ![_userNameTextfield.text length] || [_userNameTextfield.text length] < 6) {
+        [HDM popHlintMsg:@"请输入密码[长度6-16位]!"];
+        return;
+    }
+    [HHM postLogin:@{@"account": _userNameTextfield.text,
+                     @"password": _passWordTextfield.text} success:^(StatusInfoModel *status, UserInfoModel *userInf) {
+                         
+                     } failure:^(NSError *error) {
+                         [HDM errorPopMsg:error];
+                     }];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
--- ChildrenTemp/Other/Common.h
+++ ChildrenTemp/Other/Common.h
@@ -8,6 +8,22 @@
 
 #ifndef ChildrenTemp_Common_h
 #define ChildrenTemp_Common_h
-
+#import "HolterChildDataManager.h"
+#import "ChildHTTPSessionManager.h"
+#define HHM  ((ChildHTTPSessionManager*)[ChildHTTPSessionManager shareChildHTTPSessionManager])
+#define HDM  ((HolterChildDataManager*)[HolterChildDataManager sharedHolterChildDataManagerInstance])
+/**
+ *  Data Http Request
+ */
+#define HTTPDataAnalysisError @"数据错误"
+#define HTTPConnectSeverError @"无网络，请连接网络"
+#define HTTPConnectOutOfTime  @"网络异常，请求超时"
+#define HTTPConnectFaildError @"网络异常，请求失败"
+//基类地址
+#define kHttpRequestPath @"http://test.tmholter.com/baby/index.php/Interface/"
 #define kcolorWithRGB(r,g,b) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1.0]
+//日志输出宏定义
+#define MyLog(...) NSLog(__VA_ARGS__)
+#else
+#define MyLog(...)
 #endif
